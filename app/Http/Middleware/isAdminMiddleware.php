2d0e1d8f43e36a4af6d8d270de9a67fad065429d
jigarpatel <jigar.patel@planleft.com>
2023-04-19 00:11:09 +0530

    setup isAdminMiddleware

---
+++ app/Http/Middleware/isAdminMiddleware.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class isAdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param \Closure(Request): (Response) $next
+     * @return Response
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (auth()->user() && auth()->user()->is_admin) {
+            return $next($request);
+        }
+
+        abort(403);
+    }
+}
