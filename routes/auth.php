57e1658123677baa30e97ae6034228e5d14340af
jigarpatel <jigar.patel@planleft.com>
2023-04-18 23:59:15 +0530

    Socialite install with add social login button

--- routes/auth.php
+++ routes/auth.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Auth\PasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
+use App\Http\Controllers\Auth\SocialiteController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
@@ -33,6 +34,13 @@ Route::middleware('guest')->group(function () {
 
     Route::post('reset-password', [NewPasswordController::class, 'store'])
                 ->name('password.store');
+
+    // Socialite
+    Route::get('auth/{provider}/redirect', [SocialiteController::class, 'loginSocial'])
+        ->name('socialite.auth');
+
+    Route::get('auth/{provider}/callback', [SocialiteController::class, 'callbackSocial'])
+        ->name('socialite.callback');
 });
 
 Route::middleware('auth')->group(function () {
